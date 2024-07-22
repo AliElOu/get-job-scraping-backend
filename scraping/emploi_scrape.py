219ea1defd229ae29eff26fe5ffd41e82705bb55
AliElOu <alielouankrimi@gmail.com>
2024-07-22 13:42:45 +0100

    finish scraping from empoi

--- scraping/emploi_scrape.py
+++ scraping/emploi_scrape.py
@@ -1,10 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time
+import base64
 
 chrome_options = Options()
-#chrome_options.add_argument("--headless")  
+chrome_options.add_argument("--headless")  
+
+def encode_link(link):
+    encoded_bytes = base64.urlsafe_b64encode(link.encode("utf-8"))
+    encoded_str = str(encoded_bytes, "utf-8")
+    return encoded_str
+
+def decode_link(encoded_str):
+    decoded_bytes = base64.urlsafe_b64decode(encoded_str)
+    decoded_str = str(decoded_bytes, "utf-8")
+    return decoded_str
 
 
 def scrap_jobs():
@@ -26,12 +36,13 @@ def scrap_jobs():
         job_text = job_text + f"\n{job_img}" + f"\n{job_link}"
         jobs.append(job_text)
     driver.quit()
-    return jobs
+    return data_organization(jobs)
 
 
 def data_organization(arg):
     data = [jb.split("\n") for jb in arg]
-    output = [{'image':j[-2], 'title': j[0], 'company': j[1], 'location': [l for l in data[0] if l.split()[0] == "Région"][0], 'desc': j[2], 'link': j[-1]} for j in data]
+    output = [{'image':j[-2], 'title': j[0], 'company': j[1], 'location': [l for l in j if l.split()[0] == "Région"][0], 'desc': j[2], 'link': encode_link(j[-1])} for j in data]
     return output
 
-print(data_organization(scrap_jobs()))
+if __name__ == '__main__': 
+    decode_link("aHR0cHM6Ly93d3cuZW1wbG9pLm1hL29mZnJlLWVtcGxvaS1tYXJvYy9idXNpbmVzcy1kZXZlbG9wZXItaGYtbWFycmFrZWNoLTgzODAyODk")
--- scraping/scrape_jobs.py
+++ scraping/scrape_jobs.py
@@ -1,20 +1,26 @@
 import sys
 import os
+import emploi_scrape
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from firebase.firebase_config import db
 
 
 def scrape_jobs():
-    pass
+    jobs = emploi_scrape.scrap_jobs()
+    return jobs
 
 
 def save_to_firestore(jobs):
     for job in jobs:
-        job_ref = db.collection('jobs').document(job['link'])
-        if not job_ref.get().exists:
-            job_ref.set(job)
-        else:
-           print("duplicated")
+        try:
+            job_ref = db.collection('jobs').document(job['link'])
+            if not job_ref.get().exists:
+                job_ref.set(job)
+            else:
+                print("duplicated")
+        except:
+            print("Error !")
+        
 
 if __name__ == '__main__':
     jobs = scrape_jobs()
