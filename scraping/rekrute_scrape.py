6a521c3c26967803aea5551eb1148c50d26ee6f4
AliElOu <alielouankrimi@gmail.com>
2024-07-24 22:52:31 +0100

    scrap data from many pages : rekrute

--- scraping/rekrute_scrape.py
+++ scraping/rekrute_scrape.py
@@ -14,22 +14,23 @@ def encode_link(link):
 def scrap_jobs(): 
     driver = webdriver.Chrome(options=chrome_options)
     jobs = []
-    driver.get(f'https://www.rekrute.com/fr/offres.html?s=3&p=1&o=1')   
-    for _ in range(1,51):
-        job = driver.find_elements(By.XPATH, f'/html/body/div[2]/div[1]/div[2]/div/div/div/div[3]/div/div[3]/ul/li[{_}]')
-        job_link = driver.find_elements(By.XPATH, f'/html/body/div[2]/div[1]/div[2]/div/div/div/div[3]/div/div[3]/ul/li[{_}]/div/div[2]/div/h2/a') 
-        if job_link[0].text == "ExeKutive":
-            job_link = driver.find_elements(By.XPATH, f'/html/body/div[2]/div[1]/div[2]/div/div/div/div[3]/div/div[3]/ul/li[{_}]/div/div[2]/div/h2/a[2]') 
-        job_link = job_link[0].get_attribute('href')
+    for i in range(1,3):
+        driver.get(f'https://www.rekrute.com/fr/offres.html?s=3&p={i}&o=1')   
+        for _ in range(1,51):
+            job = driver.find_elements(By.XPATH, f'/html/body/div[2]/div[1]/div[2]/div/div/div/div[3]/div/div[3]/ul/li[{_}]')
+            job_link = driver.find_elements(By.XPATH, f'/html/body/div[2]/div[1]/div[2]/div/div/div/div[3]/div/div[3]/ul/li[{_}]/div/div[2]/div/h2/a') 
+            if job_link[0].text == "ExeKutive":
+                job_link = driver.find_elements(By.XPATH, f'/html/body/div[2]/div[1]/div[2]/div/div/div/div[3]/div/div[3]/ul/li[{_}]/div/div[2]/div/h2/a[2]') 
+            job_link = job_link[0].get_attribute('href')
 
-        job_img = driver.find_elements(By.XPATH, f'/html/body/div[2]/div[1]/div[2]/div/div/div/div[3]/div/div[3]/ul/li[{_}]/div/div[1]/a/img')
-        if len(job_img) == 0 :
-            job_img = driver.find_elements(By.XPATH, f'/html/body/div[2]/div[1]/div[2]/div/div/div/div[3]/div/div[3]/ul/li[{_}]/div/div[1]/img')
-        job_img = job_img[0].get_attribute('src')
-        job_text = job[0].text
-        job_text = job_text + f"\n{job_img}" + f"\n{job_link}"
-        jobs.append(job_text)
-    driver.quit()
+            job_img = driver.find_elements(By.XPATH, f'/html/body/div[2]/div[1]/div[2]/div/div/div/div[3]/div/div[3]/ul/li[{_}]/div/div[1]/a/img')
+            if len(job_img) == 0 :
+                job_img = driver.find_elements(By.XPATH, f'/html/body/div[2]/div[1]/div[2]/div/div/div/div[3]/div/div[3]/ul/li[{_}]/div/div[1]/img')
+            job_img = job_img[0].get_attribute('src')
+            job_text = job[0].text
+            job_text = job_text + f"\n{job_img}" + f"\n{job_link}"
+            jobs.append(job_text)
+    driver.quit()    
     return data_organization(jobs)
 
 def data_organization(arg):
